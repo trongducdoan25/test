c4b682b8c594ea39b067f0b1dae2fc7de32fe9af
trongducdoan25 <trongducdoan25@gmail.com>
2025-04-02 11:19:09 +0700

    2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My GIF</h1>
+      <h1>Hahahahahahahahaha</h1>
+      
+      {/* GIF */}
       <img 
         src="https://media.tenor.com/cNnsPB7dYfYAAAAM/dancing-dog.gif" 
         alt="Running GIF" 
+        className="gif-image"
       />
+      
+      {/* Scrolling Text */}
+      <div className="scrolling-text-container">
+        <div className="scrolling-text">
+          Bạn là con chó hahahahaha
+        </div>
+      </div>
     </div>
   );
 }
